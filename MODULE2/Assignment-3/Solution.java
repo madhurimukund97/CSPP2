15bbecca4211457cee6d4613ee7619b488dcdb79
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-28 14:35:54 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

--- MODULE2/Assignment-3/Solution.java
+++ MODULE2/Assignment-3/Solution.java
@@ -8,20 +8,15 @@ public class Solution {
 		Scanner s=new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
-        int result=power(base,exponent);
+        long result=power(base,exponent);
         System.out.println(result);
 	}
 	/*
 	Need to write the power function and print the output.
 	*/
 	static int power(int b, int e){
-		int res = 1;
-
-        while (e != 0)
-        {
-            res *= b;
-            --e;
-        }
-        return res;
+		if(e > 0)
+			return b*power(b,e-1);
+		return 1;
 	}
 }
