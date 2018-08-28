02f75c940657356da8ec6dc6729cacb7d63e4484
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-28 14:42:15 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- MODULE2/Assignment-3/Solution.java
+++ MODULE2/Assignment-3/Solution.java
@@ -1,3 +1,6 @@
+/*
+   Power class
+*/
 import java.util.Scanner;
 
 public class Solution {
@@ -5,18 +8,24 @@ public class Solution {
 	Do not modify this main function.
 	*/
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);      
+		/*
+          Main function
+		*/
+		Scanner s = new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
-        long result=power(base,exponent);
+        long result = power(base,exponent);
         System.out.println(result);
 	}
 	/*
 	Need to write the power function and print the output.
 	*/
 	static long power(int b, int e){
+		/*
+          Long Power
+		*/
 		if(e > 0)
-			return b*power(b,e-1);
+			return b*power(b, e-1);
 		return 1;
 	}
 }
