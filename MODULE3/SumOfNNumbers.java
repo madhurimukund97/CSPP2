e1100ad5dd360147df44e4a2c18e1d291bdf0b9d
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-29 12:22:18 +0530

    sumofn num

---
+++ MODULE3/SumOfNNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class SumOfNNumbers{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i, sum = 0;
+		for(i=1;i<=n;i++){
+			sum = sum +i;	
+		}
+		System.out.println(sum);
+	}
+}
