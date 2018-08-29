124d9cfb74435913261f22dd5d15bf5ed025d32b
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-29 12:23:07 +0530

    printusingwhile

---
+++ MODULE3/ConverttoWhileLoop.java
@@ -0,0 +1,10 @@
+class ConverttoWhileLoop{
+	public static void main(String[] args) {
+		int n = 0;
+		while(n<10){
+			n = n +2;
+			System.out.println(n);
+		}
+		System.out.println("GoodBye!");
+	}
+}
