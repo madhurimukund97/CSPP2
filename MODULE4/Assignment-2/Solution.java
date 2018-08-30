3e6de3f7f487fdcd8bf26c20e8b6c8ab56b9b0e1
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-30 14:24:35 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- MODULE4/Assignment-2/Solution.java
+++ MODULE4/Assignment-2/Solution.java
@@ -3,14 +3,15 @@ public class Solution {
     /* Fill the main function to print resultant of addition of matrices*/
     public static void main(String[] args) {
         int m, n, o, p;
+        int[][] array1, array2, sum;
         Scanner s = new Scanner(System.in);
         m = s.nextInt();
         n = s.nextInt();
         o = s.nextInt();
         p = s.nextInt();
-        int array1[][] = new int[m][n];
-        int array2[][] = new int[o][p];
-        int sum[][] = new int[m][n];
+        array1 = new int[m][n];
+        array2 = new int[o][p];
+        sum = new int[m][n];
         for (int c = 0 ; c < m ; c++) {
             for (int d = 0 ; d < n ; d++) {
                 array1[c][d] = s.nextInt();
@@ -27,7 +28,7 @@ public class Solution {
                     sum[c][d] = array1[c][d] + array2[c][d];
                 } 
             }
-            for (int c = 0; c < o; c++) {
+            for (int c = 0 ; c < o ; c++) {
                 for (int d = 0 ; d < p ; d++) {
                     if (d != n -1) {
                         System.out.print(sum[c][d] + " ");
