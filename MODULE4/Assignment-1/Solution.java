99ff6d3bb0b33b3f0c361b62d5a12665536f861f
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-30 11:59:36 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- MODULE4/Assignment-1/Solution.java
+++ MODULE4/Assignment-1/Solution.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-public class Solution
-{/*
+public final class Solution {
+/**
 Fill this main function to print maximum of given array
 
 @param      args  The arguments
 */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
         int n;
         Scanner s = new Scanner(System.in);
