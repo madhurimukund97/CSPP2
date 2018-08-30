dd8d14921ac0cef20481a94c336811b29d562cd5
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-30 14:16:45 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- MODULE4/Assignment-2/Solution.java
+++ MODULE4/Assignment-2/Solution.java
@@ -1,39 +1,46 @@
 import java.util.*;
 public class Solution {
-	/* Fill the main function to print resultant of addition of matrices*/
-	public static void main(String[] args) {
-    int m, n;
-    Scanner s = new Scanner(System.in);
-    m = s.nextInt();
-    n = s.nextInt();
- 
-    int array1[][] = new int[m][n];
-    int array2[][] = new int[m][n];
-    int sum[][] = new int[m][n];
- 
- 
-    for (int c = 0 ; c < m ; c++) {
-        for (int d = 0 ; d < n ; d++) {
-            array1[c][d] = s.nextInt();
+    /* Fill the main function to print resultant of addition of matrices*/
+    public static void main(String[] args) {
+        int m, n, o, p;
+        Scanner s = new Scanner(System.in);
+        m = s.nextInt();
+        n = s.nextInt();
+        o = s.nextInt();
+        p = s.nextInt();
+        int array1[][] = new int[m][n];
+        int array2[][] = new int[o][p];
+        int sum[][] = new int[m][n];
+        for (int c = 0 ; c < m ; c++) {
+            for (int d = 0 ; d < n ; d++) {
+                array1[c][d] = s.nextInt();
+            }
         }
-    }
-    for (int c = 0 ; c < m ; c++) {
-        for (int d = 0 ; d < n ; d++) {
-            array2[c][d] = s.nextInt();
+        for (int c = 0 ; c < o ; c++) {
+            for (int d = 0 ; d < p ; d++) {
+                array2[c][d] = s.nextInt();
+            }
         }
-    }
- 
-    for (int c = 0 ; c < m ; c++) {
-        for (int d = 0 ; d < n ; d++) {
-            sum[c][d] = array1[c][d] + array2[c][d]; 
+        if (m == o && n == p) {
+            for (int c = 0 ; c < o ; c++) {
+                for (int d = 0 ; d < p ; d++) {
+                    sum[c][d] = array1[c][d] + array2[c][d];
+                } 
+            }
+            for (int c = 0; c < o; c++) {
+                for (int d = 0 ; d < p ; d++) {
+                    if (d != n -1) {
+                        System.out.print(sum[c][d] + " ");
+                    }
+                    else {
+                        System.out.print(sum[c][d]);
+                    }
+                }
+                System.out.println();
+            }
         }
-    }
- 
-    for (int c = 0 ; c < m ; c++) {
-        for (int d = 0 ; d < n ; d++) {
-            System.out.print(sum[c][d]);
+        else {
+            System.out.println("not possible");
         }
-        System.out.println();
-    }
     }
 }
