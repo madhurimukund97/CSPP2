55fa5edbcdd63e2f6e74ee422dc43a7d900ea049
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-30 11:58:42 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- MODULE4/Assignment-1/Solution.java
+++ MODULE4/Assignment-1/Solution.java
@@ -1,28 +1,25 @@
-
-import java.util.*;
+import java.util.Scanner;
 public class Solution
 {/*
-	Fill this main function to print maximum of given array
-	*/
-	public static void main(String[] args)
-	{
-		int n;
-		Scanner s=new Scanner(System.in);
-		n= s.nextInt();
+Fill this main function to print maximum of given array
+
+@param      args  The arguments
+*/
+    public static void main(String[] args) {
+
+        int n;
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
         int a[] = new int[n];
-        for(int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             a[i] = s.nextInt();
         }
         int max = a[0];
-        for(int i = 0; i < n; i++)
-        {
-            if(max < a[i])
-            {
+        for (int i = 0; i < n; i++) {
+            if (max < a[i]) {
                 max = a[i];
             }
         }
         System.out.println(max);
-
-	}
+    }
 }
