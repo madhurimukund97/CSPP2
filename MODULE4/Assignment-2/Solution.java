0ba15eec130233dda593de836936828bd5d62d88
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-30 14:56:28 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- MODULE4/Assignment-2/Solution.java
+++ MODULE4/Assignment-2/Solution.java
@@ -19,9 +19,9 @@ public final class Solution {
      * @param      args  The arguments
      */
     public static void main(final String[] args) {
-    	/**
-    	 * main
-    	 */
+    /**
+     * main
+    */
         int m, n, o, p;
         int[][] array1, array2, sum;
         Scanner s = new Scanner(System.in);
