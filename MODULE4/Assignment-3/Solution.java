f702fbc56b91b6374b469162c581a65c287f1f98
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-31 16:52:22 +0530

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

--- MODULE4/Assignment-3/Solution.java
+++ MODULE4/Assignment-3/Solution.java
@@ -5,44 +5,44 @@ import java.util.Scanner;
  * @author madhuri.
  */
 public final class Solution {
-	/**
-	Do not modify this main function.
-	*/
-	private Solution(){
-		//constructor not used
-	}
+    /**
+    Do not modify this main function.
+    */
+    private Solution() {
+        //constructor not used
+    }
 
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.nextLine();
-		for(int i = 0; i < n; i++) {
-			String s = sc.nextLine();
-			String res = binaryToDecimal(s);
-			System.out.println(res);
-		}	
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      s     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public static String binaryToDecimal(final String s) {
-		double res = 0;
-		for (int i = 0; i < s.length(); i++) {
-			char c = s.charAt(i);
-			int number = Character.getNumericValue(c);
-			res = res + (number * Math.pow(2, (s.length() - 1) - i));
-		}
-		int result1 = (int) res;
-		String res2 = Integer.toString(result1);
-		return res2;
-	}
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            String res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String binaryToDecimal(final String s) {
+        double res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int num = Character.getNumericValue(c);
+            res = res + (num * Math.pow(2, (s.length() - 1) - i));
+        }
+        int result1 = (int) res;
+        String res2 = Integer.toString(result1);
+        return res2;
+    }
 }
