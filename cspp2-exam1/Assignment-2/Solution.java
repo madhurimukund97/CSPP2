940579093b4ccbbf8eb2b26f74fb66c902d831d3
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-09-01 11:10:50 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- cspp2-exam1/Assignment-2/Solution.java
+++ cspp2-exam1/Assignment-2/Solution.java
@@ -22,7 +22,8 @@ final class Solution {
      *
      * @return     Matrix of the rounded elements
      */
-    static int[][] roundHundred(final int[][] a, final int rows, final int columns) {
+    static int[][] roundHundred(final int[][] a,
+        final int rows, final int columns) {
 
     // write your code here
         final int max = 100;
