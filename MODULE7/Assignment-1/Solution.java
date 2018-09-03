e7de336784c7398a8c17322e8973babfa2e885e7
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-09-03 17:32:48 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- MODULE7/Assignment-1/Solution.java
+++ MODULE7/Assignment-1/Solution.java
@@ -3,21 +3,20 @@ class InputValidator {
     /*Write the atrributes and methods for InputValidator*/
     String str2;
     static int six = 6;
-    public InputValidator(String str1){
+    public InputValidator (String str1) {
         this.str2 = str1;
     }
     public boolean validateData() {
         int strlength = str2.length();
-        if(strlength >= six) {
+        if (strlength >= six) {
             return true;
-        }
-        else{
+        } else {
             return false;
         }
     }
 }
 public final class Solution {
-    public static void main(final String args[]) {
+    public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         String input = s.next();
         InputValidator i = new InputValidator(input);
