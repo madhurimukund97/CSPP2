f10535437d5dc892a4434dcde4f4b72f6f347fdb
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-09-03 17:34:55 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- MODULE7/Assignment-1/Solution.java
+++ MODULE7/Assignment-1/Solution.java
@@ -1,8 +1,11 @@
-import java.util.*;
+import java.util.Scanner;
+/**
+ * @author madhuri
+ */
 class InputValidator {
     /*Write the atrributes and methods for InputValidator*/
     String str2;
-    static int six = 6;
+    final static int six = 6;
     public InputValidator (String str1) {
         this.str2 = str1;
     }
@@ -16,7 +19,20 @@ class InputValidator {
     }
 }
 public final class Solution {
+    /**
+     * @constructor
+    */
+    private Solution() {
+        //I am not using this constructor
+    }
+    /**
+    Do not modify this main function.
+    @param      args  The arguments
+    */
     public static void main(final String[] args) {
+    /**
+     * main _ function
+    */
         Scanner s = new Scanner(System.in);
         String input = s.next();
         InputValidator i = new InputValidator(input);
