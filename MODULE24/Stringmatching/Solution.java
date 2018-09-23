b330b9af5e19456f81ac7759d4306d8da5f701e4
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-09-23 15:28:29 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

---
+++ MODULE24/Stringmatching/Solution.java
@@ -0,0 +1,129 @@
+import java.util.*;
+import java.io.*;
+
+
+class Stringmatch {
+
+	Stringmatch() {
+	}
+	public static String toString(File filename) {
+		String str = "";
+		try {
+			Scanner input = new Scanner(new FileReader(filename));
+			StringBuilder sb = new StringBuilder();
+			while(input.hasNext()) {
+				sb.append(input.next());
+				sb.append(" ");
+			}
+			input.close();
+			str = sb.toString();
+		} catch (FileNotFoundException e) {
+			System.out.println("no file");
+		}
+		return str;
+	}
+	public static Map removeAll(String text) {
+		String[] word1 = text.replaceAll("[^a-zA-Z. ]","").toLowerCase().split(" ");
+
+		//System.out.println(Arrays.toString(wordList1));
+		//return Arrays.toString(wordList1);
+		Map<String, Integer> map = new HashMap<>();
+		int f = 0;
+		for (int i = 0; i < word1.length; i++) {
+			if (!map.containsKey(word1[i])) {
+				map.put(word1[i], 1);
+			} else {
+				map.put(word1[i], map.get(word1[i]) + 1);
+			}
+		}
+		//System.out.println(map);
+		return map;
+
+
+	}
+	public static double similarString(String s1, String s2) {
+		int rows = s1.length();
+		int columns = s2.length();
+		double strLength = rows + columns;
+		int[][] max = new int[rows+1][columns+1];
+		int res = 0;
+		double sim = 0;
+		for (int i = 0; i <= rows; i++) {
+			for (int j = 0; j <= columns; j++) {
+				if (i == 0 || j == 0) {
+					max[i][j] = 0;
+				} else if (s1.charAt(i-1) == s2.charAt(j-1)) {
+					max[i][j] = max[i-1][j-1]+1;
+					//result = max(result, maximum[i][j]);
+				} else {
+					max[i][j] = 0;
+				}
+				if (res < max[i][j]) {
+					res = max[i][j];
+				}
+			}
+		}
+		//System.out.println(result);
+		sim = Math.round(((res*2)/strLength)*100D)/100D;
+		//System.out.println(stringLength);
+		return (sim*100);
+		}
+}
+
+class Solution {
+
+	public static void main(String[] args) {
+		try {
+		Stringmatch f = new Stringmatch();
+		Scanner sc = new Scanner(System.in);
+		// String line = sc.next();
+		File input = new File(sc.next());
+		File[] listoffiles = input.listFiles();
+		// for (File name:listoffiles) {
+		// 	System.out.println(name);
+		// }
+		double maximum = 0;
+		String result1 = "";
+		int length = listoffiles.length;
+		double[][] result = new double[length][length];
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < length; j++) {
+				if (i == j) {
+					result[i][j] = 100;
+				} else {
+				result[i][j] = Stringmatch.similarString(Stringmatch.toString(listoffiles[i]),Stringmatch.toString(listoffiles[j]));
+				if (maximum < result[i][j]) {
+					maximum = result[i][j];
+					result1 = "Maximum similarity is in between " + listoffiles[i].getName() + " and " + listoffiles[j].getName();
+
+				}
+			}
+			}
+		}
+		System.out.print("\t");
+		for (int i = 0; i < length; i++) {
+			System.out.print("\t" + listoffiles[i].getName());
+		}
+		System.out.println();
+		for (int i = 0; i < length; i++) {
+			System.out.print(listoffiles[i].getName() + "\t");
+			for (int j = 0; j < length; j++) {
+				System.out.print(result[i][j] + "\t\t");
+			}
+			System.out.println();
+		}
+		System.out.println(result1);
+
+
+	}catch(NoSuchElementException e) {
+		System.out.println("Empty Directory");
+	}
+
+		// String stext1 = sc.nextLine().toLowerCase();
+		// String stext2 = sc.nextLine().toLowerCase();
+		// Stringmatch feq = new Stringmatch();
+
+
+
+	}
+}
